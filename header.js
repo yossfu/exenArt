c7cd32154a9464a9e79192011700a2400453d57f
yossfu <>
2024-12-28 04:17:29 -0600

    Last Sync: 2024-12-28 10:17:29 (Mobile)

--- header.js
+++
@@ -1,7 +0,0 @@
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-container').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading header:', error));
-    
